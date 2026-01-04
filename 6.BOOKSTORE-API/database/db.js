76496a526127918b628fcf1d83f0728268110751
Azka <abdullasiddique027@gmail.com>
2026-01-04 19:21:49 +0530

    Make bookstore API Vercel serverless compatible

--- 6.BOOKSTORE-API/database/db.js
+++ 6.BOOKSTORE-API/database/db.js
@@ -1,14 +1,40 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
+// require('dotenv').config()
+
+// const connectToDB = async () => {
+//     try {
+//         const CONNECTING_STR=process.env.CONNECTING_STR;
+//         await mongoose.connect(CONNECTING_STR);
+//         console.log("mongodb is connected successfully!")
+//     } catch (error) {
+//         console.error("MongoDb connection failed", error);
+//         process.exit(1);
+//     }
+// }
+// module.exports=connectToDB;
+
+
+const mongoose = require("mongoose");
 require('dotenv').config()
+let isConnected = false;
 
 const connectToDB = async () => {
-    try {
-        const CONNECTING_STR=process.env.CONNECTING_STR;
+  if (isConnected) return;
+
+  try {
+  const CONNECTING_STR=process.env.CONNECTING_STR;
         await mongoose.connect(CONNECTING_STR);
+            isConnected = true;
         console.log("mongodb is connected successfully!")
-    } catch (error) {
-        console.error("MongoDb connection failed", error);
+
+
+  } catch (error) {
+    console.error("MongoDb connection failed", error);
         process.exit(1);
-    }
-}
-module.exports=connectToDB;
+  }
+};
+
+module.exports = connectToDB;
+
+// This prevents multiple database connections from being opened.
+// It improves stability and reliability when running on Vercel.
